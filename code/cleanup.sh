0994600d0b64b82fdaa391e230eb2f41067a7fbe
Daniel Miessler <daniel@danielmiessler.com>
2015-09-21 12:52:40 -0700

    Structure again.

--- code/cleanup.sh
+++ code/cleanup.sh
@@ -2,13 +2,20 @@
 
 # Clean up the crap entries
 cat raw.txt | sed '/\*/ d' > ./noasterisks.txt
-cat ./noasterisks.txt | sed '/\:/ d' > ./nodashes.txt
+cat ./noasterisks.txt | sed '/\:/ d' > ./nocolons.txt
 cat ./nocolons.txt | sed '/\%/ d' > ./nopercents.txt
 cat ./nopercents.txt | sed '/\-/ d' > ./nodashes.txt
-sort ./nodashes.txt | uniq > ../DisallowedDirectories.txt
+cat ./nodashes.txt | sed '/\,/ d' > ./nocommas.txt
+cat ./nocommas.txt | sed '/\_/ d' > ./nounderscores.txt
+cat ./nounderscores.txt | sed '/\?/ d' > ./noquestions.txt
+sort ./noquestions.txt | uniq > ../DisallowedDirectories.txt
 
 # Remove temporary files
 rm noasterisks.txt
+rm nodashes.txt
+rm nopercents.txt
+rm nocommas.txt
+rm nounderscores.txt
 
 echo ""
 echo "Complete."
